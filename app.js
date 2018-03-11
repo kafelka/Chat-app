19d4c1e4a68441bf80dca448cbb38b1da2a7a371
kafelka <na.hasiok@gmail.com>
2018-03-11 15:15:05 +0000

    js - toggle tab

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+const chatTab = document.querySelectorAll(".tabs li a");
+console.log(chatTab);
+
+chatTab.forEach(x => x.addEventListener("click", function toggleChatWindow() {
+  const activeTab = document.querySelector(".active");
+
+  document.querySelector("#" + activeTab.dataset.toggle).style.display = "none";
+  activeTab.classList.remove("active");
+  this.classList.add("active");
+  console.log(this.dataset.toggle);
+  document.querySelector("#" + this.dataset.toggle).style.display = "block";
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 // (function($) {
 
 // 	var tabs =  $(".tabs li a");
@@ -11,3 +53,4 @@
 // 	});
 
 // })(jQuery);
+
