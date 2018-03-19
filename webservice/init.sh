2cd152b5c4b1426e519f42203b0b397a9045b10f
kafelka <na.hasiok@gmail.com>
2018-03-19 20:04:32 +0000

    small changes

--- webservice/init.sh
+++ webservice/init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+pip install --upgrade pip
 pip install Flask
 pip install -U flask-cors
-pip install sqlite3
 
 sudo apt-get install sqlite3 libsqlite3-dev
-
-python chat.py
+rm chat.db
+sqlite3 chat.db < create.sql
